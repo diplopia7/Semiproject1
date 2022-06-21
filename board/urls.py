5d2c70fd20e0b299ce2b252c5cebf2a39e366ad9
LeeByeongHwi <diplopia7@gmail.com>
2022-06-21 12:43:52 +0900

    commit

--- board/urls.py
+++ board/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('list/',views.list, name='board/list.html'),
+    path('write/',views.write, name='board/write.html'),
+    path('view/',views.view, name='board/view.html'),
 ]
