7caea12cb52333ea6eddb5782bf56dea7eaa8e1d
LeeByeongHwi <diplopia7@gmail.com>
2022-06-23 12:04:14 +0900

    Django3 view, template 수정 - 클라이언트/서버 유효성 검사, Member 객체 생성 후 디비 저장, 저장, 가입완료시 ok페이지로 전환

--- member/views.py
+++ member/views.py
@@ -1,9 +1,38 @@
+from django.contrib.auth.hashers import make_password
 from django.shortcuts import render
 
 
 # Create your views here.
+from member.models import Member
+
+
 def join(request):
-    return render(request, 'join.html')
+    returnPage='join.html'
+
+    if request.method=='GET':
+        return render(request, returnPage)
+    elif request.method=='POST':
+        form = request.POST.dict()
+        # print(form,form['id'])
+        error = ''
+        if not (form['id'] and form['password'] and form['check_password'] and form['name'] and form['email']):
+            error='입력값이 누락되었습니다.'
+        elif form['password']!=form['check_password']:
+            error='비밀번호가 일치하지 않습니다.'
+        else:
+            member = Member(
+                userid=form['id'],
+                password=make_password(form['password']),
+                name=form['name'],
+                email=form['email']
+            )
+            member.save()
+
+            returnPage='joinok.html'
+
+        context={'form':form,'error':error}
+
+        return render(request, returnPage,context)
 
 
 def login(request):
