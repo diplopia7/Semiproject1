a8d150273ebf82ce8394aa4f4787a2fd6fc896bb
LeeByeongHwi <diplopia7@gmail.com>
2022-06-23 10:05:33 +0900

    commit

--- member/models.py
+++ member/models.py
@@ -1,3 +1,19 @@
+from datetime import datetime
+
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    id=models.AutoField(primary_key=True)
+    userid=models.CharField(max_length=18,unique=True)
+    passwd=models.CharField(max_length=18)
+    name=models.CharField(max_length=5)
+    email=models.CharField(max_length=100)
+    regdate=models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        db_table='member'
+        ordering=['-regdate']
+
+    def __str__(self):
+        return self.userid
