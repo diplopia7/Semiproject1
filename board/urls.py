5303ff208c07e88eb373afa9e49deb19164d02e8
LeeByeongHwi <diplopia7@gmail.com>
2022-06-27 17:34:48 +0900

    Django3 board 리스트/본문글 출력, 본문글 수정/삭제하기, 본문글 조회수 증가, 리스트 nocache 설정, 본문글 줄 바꿈 적용, 리스트 글 번호수정

--- board/urls.py
+++ board/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('list/',views.list, name='board/list.html'),
     path('write/',views.write, name='board/write.html'),
-    path('view/',views.view, name='board/view.html'),
+    path('view/',views.view, name='view'),
+    path('remove/',views.remove, name='board/remove.html'),
+    path('modify/',views.modify, name='board/modify.html'),
 ]
--- board/views.py
+++ board/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.views.decorators.cache import cache_control
+
 from board.models import Board
 from member.models import Member
 
-
+@cache_control(no_cache=True,no_store=True,must_revalidate=True)
 def list(request):
-    bdlist=Board.objects.values('id','title','userid','regdate','views').order_by('-regdate')
+    # bdlist=Board.objects.values('id','title','userid','regdate','views').order_by('-regdate')
+    bdlist=Board.objects.select_related('member')
+    # bdlist.get(0).member.userid
 
     context={'bds':bdlist}
 
@@ -27,7 +31,7 @@ def write(request):
             error = '입력 오류'
         else:
             board = Board(title=form['title'],contents=form['article'],
-                                      userid=Member.objects.get(pk=form['memberid'])
+                                      member=Member.objects.get(pk=form['memberid'])
                                       )
             board.save()
 
@@ -38,5 +42,48 @@ def write(request):
     return render(request, returnPage,context)
 
 
+
 def view(request):
-    return render(request, 'view.html')
+    if request.method == 'GET':
+        form = request.GET.dict()
+
+        # b = Board.objects.get(id=form['bno'])
+        # b.views += 1
+        # b.save()
+        from django.db.models import F
+        Board.objects.filter(id=form['bno']).update(views=F('views')+1)
+
+        bd = Board.objects.select_related('member').get(id=form['bno'])
+    # print(form['bno'])
+    elif request.method == 'POST':
+        pass
+
+    context={'bd':bd}
+
+    return render(request, 'view.html',context)
+
+
+def remove(request):
+    if request.method == 'GET':
+        form=request.GET.dict()
+        Board.objects.filter(id=form['bno']).delete()
+    return redirect('/list')
+
+
+def modify(request):
+    if request.method == 'GET':
+        form=request.GET.dict()
+        bd = Board.objects.get(id=form['bno'])
+    elif request.method == 'POST':
+        form=request.POST.dict()
+        # b= Board.objects.get(id=form['bno'])
+        # b.title=form['title']
+        # b.contents=form['contents']
+        # b.save()
+
+        Board.objects.filter(id=form['bno']).update(title=form['title'],contents=form['article'])
+
+        return redirect('/view?bno='+ form['bno'])
+
+    context = {'bd':bd}
+    return render(request,'modify.html',context)
